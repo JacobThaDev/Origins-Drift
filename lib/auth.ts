e58082088e30a591a64c2fd25e38231983a8d01c
JacobThaDev <jacobthad3v@gmail.com>
2025-12-16 04:24:02 -0600

    reverted auth to original

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,29 +13,45 @@ import { createPool } from "mysql2/promise";
 import { nextCookies } from "better-auth/next-js";
 import { passkey } from "@better-auth/passkey";
 
+const development_config = {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT ? process.env.MYSQL_PORT : 3306,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DATABASE,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+}
+
+const production_config = {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT ? process.env.MYSQL_PORT : 3306,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DATABASE,
+    ssl : {
+        ca: process.env.MYSQL_CERT ?? ''
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+}
+
 export const auth = betterAuth({
-	appName: process.env.NEXT_PUBLIC_SITE_NAME as string,
-    baseURL: process.env.NEXT_PUBLIC_SITE_URL as string,
+	appName: process.env.NEXT_PUBLIC_SITE_NAME,
+    baseURL: process.env.NEXT_PUBLIC_SITE_URL,
     trustedOrigins: [
-        process.env.NEXT_PUBLIC_SITE_URL as string
+        process.env.NEXT_PUBLIC_SITE_URL
     ],
-	database: createPool({
-        host: process.env.MYSQL_HOST as string,
-        port: process.env.MYSQL_PORT ? process.env.MYSQL_PORT as unknown as number : 3306,
-        user: process.env.MYSQL_USER  as string,
-        password: process.env.MYSQL_PASS  as string,
-        database: process.env.MYSQL_DATABASE  as string,
-        ssl : process.env.MYSQL_CERT == undefined ? undefined : {
-            ca: process.env.MYSQL_CERT
-        },
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-    }),
-    account: {
+	database: createPool(process.env.MYSQL_CERT == undefined ? development_config : production_config),
+	 account: {
 		accountLinking: {
 			trustedProviders: [
                 "google", 
