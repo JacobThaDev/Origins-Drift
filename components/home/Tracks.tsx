7a522cfa0b44c7ccf515d3e98693cdb39f26cf1f
JacobThaDev <jacobthad3v@gmail.com>
2025-12-17 02:24:41 -0600

    track selection on homepage

--- components/home/Tracks.tsx
+++ components/home/Tracks.tsx
@@ -6,7 +6,8 @@ import { useEffect, useState } from "react";
 import LocalApi from "@/services/LocalApi";
 import { TracksTypes } from "@/utils/types/TracksTypes";
 import { GamesTypes } from "@/utils/types/GamesTypes";
-import { ShieldCheckIcon } from "../icons/ShieldCheckIcon";
+import { BookmarkIcon } from "../icons/BookmarkIcon";
+import Link from "next/link";
 
 const TracksSection = () => {
 
@@ -41,63 +42,55 @@ const TracksSection = () => {
                     <p className="font-bold text-2xl">
                         Leaderboards
                     </p>
+
+                    {/* <div className="flex rounded items-center overflow-hidden bg-danger font-black text-sm">
+                        <div className="w-[38px] text-center">
+                            A
+                        </div>
+                        <div className="text-center border-danger border-2 border-l-0 bg-white h-full flex items-center justify-center text-black w-[48px] rounded-r">
+                            800
+                        </div>
+                    </div>
+
+                    <div className="flex rounded items-center overflow-hidden bg-[#69459C] font-black text-sm">
+                        <div className="w-[38px] text-center">
+                            S1
+                        </div>
+                        <div className="text-center border-[#69459C] border-2 border-l-0 bg-white h-full flex items-center justify-center text-black w-[48px] rounded-r">
+                            900
+                        </div>
+                    </div> */}
                 </div>
 
                 
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mb-10">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 mb-10 group">
                     {tracks && tracks.map((track:TracksTypes, index:number) => {
-                        if (!showAll && index > 3) {
+                        if (!showAll && index > 5) {
                             return null;
                         }
 
                         return(
-                            <div className="h-full bg-card rounded-2xl" key={index}>
+                            <div className="h-full bg-card rounded-2xl relative overflow-hidden transition-all duration-500" key={index}>
+                                {track.favorite && 
+                                <div className="absolute top-0 left-0 z-[4] bg-info/50 flex gap-2 items-center px-4 pe-5 py-2 rounded-br-2xl">
+                                    <BookmarkIcon height={20} className="text-info" />
+                                    <p className="text-sm font-bold">Top Track</p>
+                                </div>}
                                 <Image 
-                                    unoptimized 
                                     src={track.track_image} 
                                     width={1920}
                                     height={1080}
                                     alt=""
-                                    className={`rounded-2xl h-auto w-full group-hover:opacity-50 hover:!opacity-100 hover:!grayscale-0 relative hover:z-[1] transition-all duration-[450ms]`}/>
+                                    className={`rounded-2xl h-auto w-full relative hover:z-[1] transition-all duration-[450ms]`}/>
 
-                                <div className="p-5 px-7">
-                                    <div className="flex justify-between gap-3 items-center mb-4">
-                                        <p className="text-white/60 text-sm">Top 5 Drifters (A-Class)</p>
-                                    </div>
-
-                                    <div className="flex justify-between gap-3 items-center">
-                                        <p className="font-bold">MikeSpike83</p>
-                                        <div className="flex items-center">
-                                            <ShieldCheckIcon height={18} className="text-info" title="verified"/>
-                                            <p>950,000</p>
-                                        </div>
-                                    </div>
-                                    <div className="flex justify-between gap-3 items-center">
-                                        <p className="font-bold">Legit-JP</p>
-                                        <p>900,000</p>
-                                    </div>
-                                    <div className="flex justify-between gap-3 items-center">
-                                        <p className="font-bold">Mephistopheles</p>
-                                        <p>850,000</p>
-                                    </div>
-                                    <div className="flex justify-between gap-3 items-center">
-                                        <p className="font-bold">CloudsRWD</p>
-                                        <p>800,000</p>
-                                    </div>
-                                    <div className="flex justify-between gap-3 items-center">
-                                        <p className="font-bold">JacobThaDev</p>
-                                        <p>750,000</p>
-                                    </div>
+                                <div className="p-3">
+                                    <Link href={`/track/${track.short_name}`}
+                                            className="inline-block text-center bg-card brightness-110 hover:brightness-125 w-full py-3 rounded-xl">
+                                        Leaderboard
+                                    </Link>
                                 </div>
                             </div>
                         )
-
-                        // return(
-                        //     <Link href={`/track/${track.short_name}`} className="inline-block w-[150px] h-[150px]" key={index}>
-                        //         <Image unoptimized src={track.track_image} width={1920} height={1080} alt=""
-                        //             className="h-auto w-[150px] rounded-2xl group-hover:opacity-50 hover:!opacity-100 hover:!grayscale-0 relative hover:z-[1] transition-all duration-[450ms] hover:"/>
-                        //     </Link>
-                        // )
                     })}
                 </div>
 
