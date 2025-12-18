80ada68443f6df9b84e4abdcd9feef03bd49ad14
JacobThaDev <jacobthad3v@gmail.com>
2025-12-18 05:51:00 -0600

    class button hover and lighter muted color

--- components/leaderboards/ClassSelector.tsx
+++ components/leaderboards/ClassSelector.tsx
@@ -11,7 +11,7 @@ const ClassSelector = () => {
             
             <button onClick={() => setClassFilter(classFilter == "a" ? "s1" : "a")}
                 disabled={loading}
-                    className="bg-card px-5 py-2.5 rounded-xl disabled:opacity-60 flex items-center gap-3">
+                    className="bg-card hover:bg-infodark px-5 py-2.5 rounded-xl disabled:opacity-60 flex items-center gap-3">
                         <p className="text-sm text-muted">Class</p>
                 {classFilter.toUpperCase()}-{classFilter == "a" ? "800" : "900"} 
             </button>
