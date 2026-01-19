1d83f5753bbfff95ae23472ca52a7782a4bbd8d6
JacobThaDev <jacobthad3v@gmail.com>
2026-01-19 11:47:53 -0600

    nodejs runtime for localapi

--- services/LocalApi.tsx
+++ services/LocalApi.tsx
@@ -1,6 +1,8 @@
 import Axios from "axios";
 import { env as _env } from 'process';
 
+export const runtime = 'nodejs';
+
 const LocalApi = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_SITE_URL+"/api",
     timeout: 10000,
